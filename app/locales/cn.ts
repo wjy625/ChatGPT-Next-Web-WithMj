ad66beeccc1feca0b3fd101fb1b6e5f85f48109c
wjy625 <136810670+wjy625@users.noreply.github.com>
2023-11-01 23:16:06 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，[设置](https://t.zsxq.com/13AMgcgzN)",
+      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，[点这里获取访问密码](https://t.zsxq.com/13AMgcgzN)",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
